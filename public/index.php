f4694603a306fa8178b08aaa20fa3455070e0a0b
spawn2010 <spawn.2010@mail.ru>
2022-02-20 20:57:17 +0300

    add new route

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Factory\AppFactory;
 
 // подключаем Composer
@@ -10,18 +11,46 @@ require dirname(__DIR__).'/vendor/autoload.php';
 // создаём объект приложения
 $app = AppFactory::create();
 
+$app->addErrorMiddleware(false, false, false);
 
-// http://demo/slim1/
+// создадим свой Middleware
+$myMiddleware1 = function (ServerRequestInterface $request, RequestHandlerInterface $handler) {
+    $response = $handler->handle($request);
+    $response->getBody()->write(' myMiddleware1 ');
+    return $response;
+};
+
+$myMiddleware2 = function (ServerRequestInterface $request, RequestHandlerInterface $handler) {
+    $response = $handler->handle($request);
+    $response->getBody()->write(' myMiddleware2');
+    return $response;
+};
+
+// добавим его в приложение
+$app->add($myMiddleware2);
+$app->add($myMiddleware1);
+
+
+// http://
 $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
     $response->getBody()->write('Home page');
     return $response;
 });
 
-// http://demo/slim1/hello
+// http://hello
 $app->get('/hello', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
     $response->getBody()->write('Hello world!');
     return $response;
 });
+
+// всё остальное
+$app->get('/{slug}', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+    $response->getBody()->write('This page is ' . $args['slug']);
+    return $response;
+});
+
+
+
 
 // запускаем приложение
 $app->run();
