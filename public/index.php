afe41e0a676bcc4e27d3aee7c2228453cca0b9d5
spawn2010 <spawn.2010@mail.ru>
2022-02-20 15:31:07 +0300

    hello world

--- public/index.php
+++ public/index.php
@@ -1,18 +1,29 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Slim\Factory\AppFactory;
 
+// подключаем Composer
 require dirname(__DIR__).'/vendor/autoload.php';
 
-
+// создаём объект приложения
 $app = AppFactory::create();
 
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+// http://demo/slim1/
+$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+    $response->getBody()->write('Home page');
     return $response;
 });
 
+// http://demo/slim1/hello
+$app->get('/hello', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+    $response->getBody()->write('Hello world!');
+    return $response;
+});
+
+// запускаем приложение
 $app->run();
+
+# end of file
