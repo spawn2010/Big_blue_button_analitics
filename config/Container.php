7202ecc6d3265926b626f2ba2daaf30ad6a02bd9
user <spawn.2010@mail.ru>
2022-02-28 16:05:26 +0300

    added BBB

--- config/Container.php
+++ config/Container.php
@@ -1,19 +1,22 @@
 <?php
 
+use BigBlueButton\BigBlueButton;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Src\DataBase;
 use function DI\autowire;
-use function DI\get;
 
 return array(
-    DataBase::class => autowire()->constructorParameter('dsn', 'mysql:host=TEST_DB;dbname=TEST')
-        ->constructorParameter('username', 'root')
-        ->constructorParameter('password', 'root'),
+    DataBase::class => autowire()->constructorParameter('dsn', getenv('DATABASE_DSN'))
+        ->constructorParameter('username', getenv('DATABASE_USERNAME'))
+        ->constructorParameter('password', getenv('DATABASE_PASSWORD')),
     Psr\Log\LoggerInterface::class => DI\factory(function () {
         $logger = new Logger('general');
         $fileHandler = new StreamHandler(dirname(__DIR__) . '/var/log/app.log', Logger::INFO);
         $logger->pushHandler($fileHandler);
         return $logger;
+    }),
+    BigBlueButton::class => DI\factory(function () {
+        return new BigBlueButton(getenv('API_BASEURL'),getenv('API_SECRET'));
     }),
 );
--- index.php
+++ index.php
@@ -3,22 +3,28 @@
 const BASE_DIR = __DIR__;
 
 use App\Middleware\LogMiddleware;
+use BigBlueButton\BigBlueButton;
 use DI\ContainerBuilder;
 use Psr\Log\LoggerInterface;
 use Slim\Factory\AppFactory;
+use DevCoder\DotEnv;
 
 require __DIR__ . '/vendor/autoload.php';
+
+(new DotEnv(__DIR__ . '/local.env'))->load();
 
 $builder = new ContainerBuilder();
 $builder->addDefinitions('config/Container.php');
 
-$container2 = $builder->build();
-AppFactory::setContainer($container2);
+$container = $builder->build();
+AppFactory::setContainer($container);
+
+$bbb = $container->get(BigBlueButton::class);
 
 $app = AppFactory::create();
 $app->add(LogMiddleware::class);
 
-$errorMiddleware = $app->addErrorMiddleware(true, true, true, $container2->get(LoggerInterface::class));
+$errorMiddleware = $app->addErrorMiddleware(true, true, true, $container->get(LoggerInterface::class));
 
 require __DIR__.'/config/Routes.php';
 
