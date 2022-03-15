fc1550a58fc922452cc29a845652b04c24bad745
user <spawn.2010@mail.ru>
2022-03-15 13:12:35 +0300

    add doctrine Dbal

--- app/dependencies.php
+++ app/dependencies.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Settings\SettingsInterface;
 use BigBlueButton\BigBlueButton;
 use DI\ContainerBuilder;
+use Doctrine\DBAL\DriverManager;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Processor\UidProcessor;
@@ -29,6 +30,15 @@ return function (ContainerBuilder $containerBuilder) {
         },
         BigBlueButton::class => DI\factory(function () {
             return new BigBlueButton(getenv('API_BASEURL'),getenv('API_SECRET'));
+        }),
+        DriverManager::class => DI\factory(function () {
+            return DriverManager::getConnection([
+                'dbname' => getenv('DATABASE_NAME'),
+                'user' => getenv('DATABASE_USERNAME'),
+                'password' => getenv('DATABASE_PASSWORD'),
+                'host' => getenv('DATABASE_HOST'),
+                'driver' => getenv('DATABASE_DRIVER'),
+            ]);
         }),
     ]);
 
