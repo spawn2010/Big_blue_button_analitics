53d0096cf88e2e2f57d04d3b6ff23e47524ce5d8
user <spawn.2010@mail.ru>
2022-03-18 19:41:27 +0300

    code style

--- src/Action/RefreshAction.php
+++ src/Action/RefreshAction.php
@@ -3,7 +3,6 @@
 namespace App\Action;
 
 use App\Service\WriteService;
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
