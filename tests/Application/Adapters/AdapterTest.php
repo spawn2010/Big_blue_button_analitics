5c82325f012b42809fef4e2c41b1893ffaa8fa5c
user <spawn.2010@mail.ru>
2022-03-25 15:03:28 +0300

    add AdapterTest

---
+++ tests/Application/Adapters/AdapterTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Application\Adapters;
+
+use App\Adapter\Adapter;
+use App\Entity\Meeting;
+use PHPUnit\Framework\TestCase;
+
+class AdapterTest extends TestCase
+{
+    private Adapter $adapter;
+    private $objectFromApi;
+
+    public function setUp(): void
+    {
+
+        $xml =  simplexml_load_string(file_get_contents(dirname(__DIR__).'/../fixtures/get_meeting_info.xml'));
+        $this->objectFromApi = (new \BigBlueButton\Responses\GetMeetingInfoResponse($xml))->getMeeting();
+        $this->adapter = new Adapter($this->objectFromApi,[],Meeting::class);
+    }
+
+    public function testToEntity()
+    {
+        $this->assertInstanceOf(Meeting::class,$this->adapter->toEntity());
+        $info = $this->adapter->toEntity();
+        $this->assertEquals('Mock meeting for testing getMeetingInfo API method', $info->getMeetingName());
+        $this->assertEquals('117b12ae2656972d330b6bad58878541-28-15', $info->getMeetingId());
+        $this->assertEquals('178757fcedd9449054536162cdfe861ddebc70ba-1453206317376', $info->getInternalMeetingId());
+        $this->assertEquals(1453206317380, $info->getStartTime());
+        $this->assertEquals('Tue Jan 19 07:25:17 EST 2016', $info->getCreationDate());
+        $this->assertEquals(true, $info->getRunning());
+        $this->assertEquals(1453206317376, $info->getCreationTime());
+        $this->assertEquals(1453206325002, $info->getEndTime());
+        $this->assertEquals(20, $info->getDuration());
+        $this->assertEquals(20, $info->getMaxUsers());
+        $this->assertCount(4, $info->getAttendees());
+    }
+}
